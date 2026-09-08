f2807bcf2ad1d09fa50866815c7819b30118caa9
FiresonZ <dev@gith.uk>
2026-09-08 15:31:30 +0000

    Revert "fix(plugins): 注册 KAGParser.dll 兼容名，修复请求标准名 KAG 解析器的游戏(IINCHO)二次/新开黑屏"
    This reverts commit a162c71eea1d7e9d22fd022eb588f2ea31578859.

--- cpp/plugins/kagparserex/kagparserex_plugin.cpp
+++ cpp/plugins/kagparserex/kagparserex_plugin.cpp
@@ -4,16 +4,9 @@
 static void LinkKAGParserCompatibility() {}
 static void UnlinkKAGParserCompatibility() {}
 
-// Register three compatibility module names:
-// - KAGParser.dll (standard/plain name used by many distributions)
+// Register two compatibility module names:
 // - KAGParserEx.dll (wamsoft)
 // - ExtKAGParser.dll (older scripts)
-static ncbCallbackAutoRegister g_kagparser_cb(
-    TJS_W("KAGParser.dll"), ncbAutoRegister::PreRegist,
-    &LinkKAGParserCompatibility, nullptr);
-static ncbCallbackAutoRegister g_kagparser_unload_cb(
-    TJS_W("KAGParser.dll"), ncbAutoRegister::PostRegist, nullptr,
-    &UnlinkKAGParserCompatibility);
 static ncbCallbackAutoRegister g_kagparserex_cb(
     TJS_W("KAGParserEx.dll"), ncbAutoRegister::PreRegist,
     &LinkKAGParserCompatibility, nullptr);
